0584b064c78ccbcecfeb43520088533ab8b3344f
aalifaalia <riyaz5mrs@gmail.com>
2022-10-11 15:23:52 +0530

    yousuf

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,19 +1,19 @@
 package org.sample;
 
 public class Employee {
-	
+
 	private static void companyEmployee() {
 		System.out.println("Employee Name Is : AALIF");
 	}
+
 	private static void manager() {
 		System.out.println("Manager Name Is : AALIA");
-		System.out.println("RIYAS");
 	}
-	
+
 	public static void main(String[] args) {
 		companyEmployee();
 		manager();
-		
+
 	}
 
 }
