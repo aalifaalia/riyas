0ac8b658c5b1f01b39b9f0ee6515232741537436
aalifaalia <riyaz5mrs@gmail.com>
2022-10-11 13:20:54 +0530

    git

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Employee {
+	
+	private static void companyEmployee() {
+		System.out.println("Employee Name Is : AALIF");
+	}
+	private static void manager() {
+		System.out.println("Manager Name Is : AALIA");
+	}
+	
+	public static void main(String[] args) {
+		companyEmployee();
+		manager();
+		
+	}
+
+}
---
+++ src/org/sample/Sample1.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample1 {
+	public static void main(String[] args) {
+	//To Insert Value
+	List<Integer>l = new ArrayList<>();
+	l.add(0);
+	l.add(1);
+	l.add(0);
+	l.add(1);
+	l.add(0);
+	l.add(0);
+	l.add(1);
+	l.add(1);
+	l.add(1);
+	l.add(0);
+	System.out.println(l);
+	
+		
+		
+		
+		
+	}
+	
+}
